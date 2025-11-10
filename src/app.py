e6d0cfd5ee46a5347b558d513d8eefbd11136fa7
Yoalvarado25 <manacima2019@gmail.com>
2025-11-10 18:33:30 +0000

    commit storejs

--- src/app.py
+++ src/app.py
@@ -2,14 +2,24 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from flask_socketio import SocketIO
+from api.sockets.handlers import register_socket_handlers
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
+from api.mail_config import mail
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
+import cloudinary
+from flask_cors import CORS
+from api.routes.user_routes import user_bp
+from api.routes.rating_routes import rating_bp
+from datetime import timedelta
+
 
 # from models import Person
 
@@ -18,6 +28,17 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+CORS(app, resources={
+     r"/api/*": {"origins": os.getenv('VITE_FRONTEND_URL')}}, supports_credentials=True)
+app.register_blueprint(user_bp)
+app.register_blueprint(rating_bp, url_prefix='/api')
+
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY','change-me')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=2)
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -57,6 +78,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -65,8 +88,42 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+# Setup the Flask-Mail-Extended extension
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+
+mail.init_app(app)
+
+# cloudinary
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    secure=True
+)
+
+# Scoketio
+app.config['SECRET_KEY'] = os.getenv('SOCKET_SECRET_KEY')
+
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    path="/socket.io",
+    logger=True,
+    engineio_logger=False,
+)
+
+register_socket_handlers(socketio, app)
+
+# # this only runs if `$ python src/main.py` is executed
+# if __name__ == '__main__':
+#     PORT = int(os.environ.get('PORT', 3001))
+#     socketio.run(app, host='0.0.0.0', port=PORT, debug=True)
+
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,17 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
 
-from app import app as application
+# from app import app as application
+
+# if __name__ == "__main__":
+#     application.run()
+
+import eventlet
+eventlet.monkey_patch()
+
+import os
+from app import app, socketio
 
 if __name__ == "__main__":
-    application.run()
+    PORT = int(os.environ.get("PORT", 3001))
+    socketio.run(app, host="0.0.0.0", port=PORT)
